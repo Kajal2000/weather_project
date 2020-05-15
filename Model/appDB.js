3513ff3031ee4b3368c89a81eabf22e294bab83a
kajal <you@example.com>
2020-05-15 17:56:46 +0530

    updated

--- Model/appDB.js
+++ Model/appDB.js
@@ -2,11 +2,13 @@ const knex = require('../connection.js');
 
 // post Data
 
-let post_data = (store_data) => {
-    console.log(store_data);
-    
+let post_data = (store_data) => {    
     return knex("weather").insert(store_data)
 }
 
+let search_data = (search)=>{
+    return knex.select("*")
+    .from("weather").where("State","like","%" + search+ "%")
+}
 
-module.exports = {post_data}
+module.exports = {post_data,search_data}
--- Routes/api.js
+++ Routes/api.js
@@ -3,33 +3,46 @@ const app = express();
 const appDB = require("../Model/appDB");
 
 
-const request = require("request")
-var readline = require('readline-sync');
-var user_input = readline.question("Enter your satate Name ?")
-request("http://api.weatherstack.com/current?access_key=b3d1212d49a9746ce06fbacb533c9d93&query=("+user_input+")",
-function(err,res,body){
-var allData = JSON.parse(body)
-console.log(allData)
+// const request = require("request")
+// var readline = require('readline-sync');
+// var user_input = readline.question("Enter your satate Name ?")
+// request("http://api.weatherstack.com/current?access_key=b3d1212d49a9746ce06fbacb533c9d93&query=("+user_input+")",
+// function(err,res,body){
+// var allData = JSON.parse(body)
+// // console.log(allData)
 
 // var region = allData['location']['region']
 // var temperature = allData["current"]["temperature"] 
 // console.log(region,temperature)
 
-// app.post("/postApi",(req,res)=>{
-//     var store_data = {
-//         id : req.body.id,
-//         State : region,
-//         Temperature : temperature
-//     }
-//     appDB.post_data(store_data)
-//     .then(()=>{
-//         res.send("inserted data")
-//     })
-//     .catch((err)=>{
-//         res.send(err)
-//     })
-// })
+app.post("/postApi",(req,res)=>{
+    var store_data = {
+        id : req.body.id,
+        State : region,
+        Temperature : temperature
+    }
+    appDB.post_data(store_data)
+    .then(()=>{
+        res.send("inserted data")
+    })
+    .catch((err)=>{
+        res.send(err)
+    })
 })
+
+
+app.get("/getApi/:search",(req,res)=>{
+    var search = req.params.search
+    appDB.search_data(search)
+    .then((res_data)=>{
+        res.send(res_data)
+    }).catch((err)=>{
+        res.send(err)
+    })
+
+})
+// })
+
 
 
 module.exports = app;
