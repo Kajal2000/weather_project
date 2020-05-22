6db67dcb5fa7759b24cd9a15b3a7d2e0f56c1dde
kajal <you@example.com>
2020-05-23 00:20:18 +0530

    updated

--- weather.js
+++ weather.js
@@ -7,8 +7,8 @@ state_List = ["Andhra Pradesh","Arunachal Pradesh","Assam","Bihar","Chhattisgarh
 "Mizoram","	Nagaland","	Odisha","Punjab","Rajasthan","Sikkim","Tamil Nadu","Telangana","Tripura","Uttar Pradesh","Uttarakhand","West Bengal"]
 
 captial_List = ["Amaravati", "Itanagar", "Dispur", "Patna", "Naya Raipur", "Panaji", "Gandhinagar", "Chandigarh", "Shimla", "Ranchi", "Bengaluru",
-  "Thiruvananthapuram", "Bhopal", "Mumbai", "Imphal", "Shillong", "Aizawl", "Kohima", "Bhubaneswar",
-  "Chandigarh", "Jaipur", "Gangtok", "Chennai", "Hyderabad", "Agartala", "Lucknow", "Dehradun", "	Kolkata"]
+"Thiruvananthapuram", "Bhopal", "Mumbai", "Imphal", "Shillong", "Aizawl", "Kohima", "Bhubaneswar",
+"Chandigarh", "Jaipur", "Gangtok", "Chennai", "Hyderabad", "Agartala", "Lucknow", "Dehradun", "	Kolkata"]
 
 
 app.get("/getApi",(req,res)=>{
